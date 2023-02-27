e63711e724d4a76fb3fe2170e3f81815256c7128
flaars <fcateloiu08@gmail.com>
2023-02-27 16:11:10 +0100

    Initial commit

---
+++ src/test/java/com/example/examensegundaevaluacion/ExamenSegundaEvaluacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.examensegundaevaluacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenSegundaEvaluacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
